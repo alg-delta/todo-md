3235be24597c4d639b21d11cef6eac6b54c514e2
LarysaHliieva <lora.glievaya@gmail.com>
2024-12-16 17:31:59 +0200

    fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,50 +1,51 @@
-import React from 'react'
-import CreateTask from './CreateTask'
-import { useState, useEffect } from 'react';
-import TaskList from './TaskList';
+import React from "react";
+import CreateTask from "./CreateTask";
+import { useState, useEffect } from "react";
+import TaskList from "./TaskList";
 
 export default function Main() {
   const [list, setList] = useState(() => {
-    return JSON.parse(localStorage.getItem("savedList")) ?? [];
+    return JSON.parse(localStorage.getItem("savedListMD")) ?? [];
   });
   const addTask = (task, priority) => {
-const newTask = {
-  id: Math.floor(Math.random() * 9000) + 1000,
-  text: task,
-  isComplete: false,
-  priority: priority,
-};
-setList([...list, newTask]);
+    const newTask = {
+      id: Math.floor(Math.random() * 9000) + 1000,
+      text: task,
+      isComplete: false,
+      priority: priority,
+    };
+    setList([...list, newTask]);
   };
- 
 
   const deleteTask = (id) => {
-    console.log("Видалення",id)
-    const newList = list.filter((task)=>task.id !== id);
+    console.log("Видалення", id);
+    const newList = list.filter((task) => task.id !== id);
     setList(newList);
-
   };
 
   const completeTask = (id) => {
-    console.log("Виконана",id)
-    const newList = list.map(task=>{
-      if(task.id === id) {
-        return {...task, isComplete:  !task.isComplete}
+    console.log("Виконана", id);
+    const newList = list.map((task) => {
+      if (task.id === id) {
+        return { ...task, isComplete: !task.isComplete };
       } else {
         return task;
       }
     });
-setList(newList);
+    setList(newList);
   };
   useEffect(() => {
-    localStorage.setItem("savedList", JSON.stringify(list));
-  }, [list])
+    localStorage.setItem("savedListMD", JSON.stringify(list));
+  }, [list]);
   return (
     <div className="main">
-        <h3 className="title">Список задач</h3>
-        <CreateTask addTask={addTask}/>
-        <TaskList list={list} deleteTask={deleteTask} completeTask={completeTask}  />
-       
+      <h3 className="title">Список задач</h3>
+      <CreateTask addTask={addTask} />
+      <TaskList
+        list={list}
+        deleteTask={deleteTask}
+        completeTask={completeTask}
+      />
     </div>
-  )
+  );
 }
